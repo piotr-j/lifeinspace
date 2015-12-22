629ddd23b858a275b73745eabcfc9736c438d6e6
PiotrJ <me@piotrjastrzebski.io>
2015-12-22 18:01:18 +0100

    added map loading

--- core/src/io/piotrjastrzebski/lis/utils/Assets.kt
+++ core/src/io/piotrjastrzebski/lis/utils/Assets.kt
@@ -1,6 +1,8 @@
 package io.piotrjastrzebski.lis.utils
 
 import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.kotcrab.vis.ui.VisUI
 
@@ -9,12 +11,16 @@ import com.kotcrab.vis.ui.VisUI
  */
 class Assets(val pixelScaleFactor: Float) {
     protected lateinit var skin: Skin get
+    protected lateinit var map :TiledMap
     private val SKIN_PATH = "gui/uiskin.json"
+    private val MAP_PATH = "map.tmx"
     private val assetManager: AssetManager
 
     init {
         assetManager = AssetManager()
+        assetManager.setLoader(TiledMap::class.java, TmxMapLoader())
         assetManager.load<Skin>(SKIN_PATH, Skin::class.java)
+        assetManager.load<TiledMap>(MAP_PATH, TiledMap::class.java)
         // TODO load assets and stuff
     }
 
@@ -33,6 +39,7 @@ class Assets(val pixelScaleFactor: Float) {
         VisUI.load(if (pixelScaleFactor > 1.5f) VisUI.SkinScale.X2 else VisUI.SkinScale.X1)
 
         skin = assetManager.get(SKIN_PATH, Skin::class.java)
+        map = assetManager.get(MAP_PATH, TiledMap::class.java)
     }
 
     fun dispose() {
