699f71a4bdeb23ff4a7488d830ec793cdbd57b14
EvilEntity <me@piotrjastrzebski.io>
2015-12-26 20:40:58 +0100

    added some neat extensions

--- core/src/io/piotrjastrzebski/lis/game/Extensions.kt
+++ core/src/io/piotrjastrzebski/lis/game/Extensions.kt
@@ -1,5 +1,21 @@
 package io.piotrjastrzebski.lis.game
 
+import com.artemis.Component
+import com.artemis.EntityEdit
+import com.artemis.World
+
 /**
  * Created by EvilEntity on 21/12/2015.
  */
+/**
+ * Now that's a mouthful!
+ *
+ * Usage:
+ * edit.create<Transform>()
+ * will be translated to
+ * edit.create(Transform::class.java)
+ */
+inline fun <reified T: Component> EntityEdit.create(): T = this.create(T::class.java)
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun World.createAndEdit(): EntityEdit = this.createEntity().edit()
