0787c2379df96bf8b6d111169f16ccc9e5884bdd
PiotrJ <me@piotrjastrzebski.io>
2016-01-11 17:15:25 +0100

    cleaned up old debug crap

--- core/src/io/piotrjastrzebski/lis/game/processors/ModelRenderer.kt
+++ core/src/io/piotrjastrzebski/lis/game/processors/ModelRenderer.kt
@@ -42,15 +42,10 @@ class ModelRenderer() : IteratingSystem(Aspect.all(RenderableModel::class.java).
     @Wire lateinit var assets: Assets
     @Wire lateinit var physics: Physics
 
-    internal val GROUND_FLAG = 1 shl 8
-    internal val OBJECT_FLAG = 1 shl 9
-    internal val ALL_FLAG = -1
-
     val modelBatch = ModelBatch()
     val environment = Environment()
-    var instances = com.badlogic.gdx.utils.Array<ModelInstance>()
-    private var debugInstances = com.badlogic.gdx.utils.Array<GameObject>()
 
+    var instances = com.badlogic.gdx.utils.Array<ModelInstance>()
     var debugCamera = PerspectiveCamera(67f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
     var camController = CameraInputController(debugCamera)
 
@@ -65,139 +60,8 @@ class ModelRenderer() : IteratingSystem(Aspect.all(RenderableModel::class.java).
 
     private lateinit var debugFrustumModel: Model
     private lateinit var debugFrustumInstance: ModelInstance
-    private lateinit var testModel: Model
-
-    internal class GameObject(model: Model, node: String, constructionInfo: btRigidBody.btRigidBodyConstructionInfo) : ModelInstance(model, node), Disposable {
-        val body: btRigidBody
-        val motionState: BulletMotionState
-
-        init {
-            motionState = BulletMotionState()
-            motionState.transform = transform
-            body = btRigidBody(constructionInfo)
-            body.motionState = motionState
-        }
-
-        override fun dispose() {
-            body.dispose()
-            motionState.dispose()
-        }
-
-        internal class Constructor(val model: Model, val node: String, val shape: btCollisionShape, mass: Float) : Disposable {
-            val constructionInfo: btRigidBody.btRigidBodyConstructionInfo
-
-            init {
-                if (mass > 0f)
-                    shape.calculateLocalInertia(mass, localInertia)
-                else
-                    localInertia.set(0f, 0f, 0f)
-                constructionInfo = btRigidBody.btRigidBodyConstructionInfo(mass, null, shape, localInertia)
-            }
-
-            fun construct(): GameObject {
-                return GameObject(model, node, constructionInfo)
-            }
-
-            override fun dispose() {
-                shape.dispose()
-                constructionInfo.dispose()
-            }
-            // static vector. fun
-            companion object {
-                private val localInertia = Vector3()
-            }
-        }
-    }
-
-    private var constructors = ArrayMap<String, GameObject.Constructor>(String::class.java, GameObject.Constructor::class.java)
-
-    private lateinit var ground: GameObject
-
-    fun spawn() {
-        val obj = constructors.values[1].construct()
-        obj.transform.setFromEulerAngles(MathUtils.random(360f), MathUtils.random(360f), MathUtils.random(360f))
-        obj.transform.trn(MathUtils.random(-1.5f, 1.5f), MathUtils.random(-1.5f, 1.5f), 6f)
-        obj.body.proceedToTransform(obj.transform)
-        obj.body.userValue = debugInstances.size
-        obj.body.collisionFlags = obj.body.collisionFlags or btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK
-        debugInstances.add(obj)
-        physics.addRigidBody(obj.body)
-        obj.body.contactCallbackFlag = OBJECT_FLAG
-        obj.body.contactCallbackFilter = GROUND_FLAG
-    }
-
-    fun createStuff(name: String, model: Model, meshParts: Array<MeshPart>) {
-        val meshPart = model.getNode("$name-col").parts.first().meshPart
-        meshParts.clear()
-        meshParts.add(meshPart)
-        constructors.put(name, GameObject.Constructor(model, name, btBvhTriangleMeshShape(meshParts), 0f))
-    }
-
-    val meshParts = Array<MeshPart>()
-    public fun createTile(name: String, pos:Vector3, yRot: Float) {
-        var constructor:GameObject.Constructor? = constructors.get(name)
-        if (constructor == null) {
-            meshParts.clear()
-            // apparently this works somehow
-            meshParts.add(assets.tilesModel.getNode(name).parts.first().meshPart)
-            constructor = GameObject.Constructor(assets.tilesModel, name, btBvhTriangleMeshShape(meshParts), 0f)
-            constructors.put(name, constructor)
-        }
-        val obj = constructor.construct()
-        obj.transform.trn(pos)
-        obj.transform.rotate(Vector3.Z, yRot)
-        obj.body.proceedToTransform(obj.transform)
-        obj.body.userValue = debugInstances.size
-        obj.body.collisionFlags = obj.body.collisionFlags or btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK
-        debugInstances.add(obj)
-        physics.addRigidBody(obj.body)
-    }
 
     override fun initialize() {
-        val mb = ModelBuilder()
-        mb.begin();
-        mb.node().id = "ground";
-        mb.part("ground", GL20.GL_TRIANGLES,
-                (VertexAttributes.Usage.Position or VertexAttributes.Usage.Normal).toLong(),
-                Material(ColorAttribute.createDiffuse(Color.RED)))
-                .box(10f, 10f, 1f);
-        mb.node().id = "sphere";
-        mb.part(
-                "sphere", GL20.GL_TRIANGLES,
-                (VertexAttributes.Usage.Position or VertexAttributes.Usage.Normal).toLong(),
-                Material(ColorAttribute.createDiffuse(Color.GREEN)))
-                .sphere(1f, 1f, 1f, 10, 10);
-        testModel = mb.end();
-
-        constructors.put("ground", GameObject.Constructor(testModel, "ground", btBoxShape(Vector3(5f, 5f, .5f)), 0f))
-        constructors.put("sphere", GameObject.Constructor(testModel, "sphere", btSphereShape(0.5f), 1f))
-
-        ground = constructors.get("ground").construct()
-        physics.addRigidBody(ground.body)
-        ground.body.contactCallbackFlag = GROUND_FLAG
-        ground.body.contactCallbackFilter = 0
-        debugInstances.add(ground)
-
-
-        // neat works!
-        createStuff("tile-pyramid", assets.tilesModel, meshParts)
-
-        meshParts.clear()
-        // apparently this works somehow
-        meshParts.add(assets.tilesModel.getNode("tile-corner-in-col").parts.first().meshPart)
-        constructors.put("tile-corner-in", GameObject.Constructor(assets.tilesModel, "tile-corner-in", btBvhTriangleMeshShape(meshParts), 0f))
-
-        val obj = constructors.get("tile-corner-in").construct()
-        obj.transform.trn(0f, 0f, 3f)
-        obj.body.proceedToTransform(obj.transform)
-        obj.body.userValue = debugInstances.size
-        obj.body.collisionFlags = obj.body.collisionFlags or btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK
-        debugInstances.add(obj)
-        physics.addRigidBody(obj.body)
-//        obj.body.contactCallbackFlag = OBJECT_FLAG
-//        obj.body.contactCallbackFilter = GROUND_FLAG
-
-
         keybinds.register(this, Input.Keys.F4, {toggleDebug()}, {false})
         currentCamera = camera
 
@@ -215,6 +79,7 @@ class ModelRenderer() : IteratingSystem(Aspect.all(RenderableModel::class.java).
         camera.position.z = 10f
         camera.lookAt(0f, 0f, 0f)
         camera.update()
+
         debugFrustumModel = createFrustumModel(camera)
         debugFrustumInstance = ModelInstance(debugFrustumModel)
 
@@ -235,19 +100,10 @@ class ModelRenderer() : IteratingSystem(Aspect.all(RenderableModel::class.java).
         environment.add(DirectionalLight().set(0.8f, 0.8f, 0.8f, 1f, -0.8f, -0.2f))
     }
 
-    private var spawnTimer = 0f;
-
     override fun render() {
-        spawnTimer -= world.delta
-        if (spawnTimer < 0) {
-            spawn()
-            spawnTimer = 1.5f
-        }
-
         camController.update()
         modelBatch.begin(currentCamera)
         modelBatch.render<ModelInstance>(instances, environment)
-        modelBatch.render<ModelInstance>(debugInstances, environment)
         if (debug) {
             debugFrustumInstance.transform.set(camera.view).inv()
             modelBatch.render(debugFrustumInstance, environment)
@@ -313,6 +169,7 @@ class ModelRenderer() : IteratingSystem(Aspect.all(RenderableModel::class.java).
     override fun resize(width: Int, height: Int) {
         debugFrustumModel.dispose()
         // lets hope whatever is using the cam will position it properly
+        val oldCombined = camera.combined.cpy()
         camera.position.x = 0f
         camera.position.y = 0f
         camera.position.z = 10f
@@ -320,5 +177,6 @@ class ModelRenderer() : IteratingSystem(Aspect.all(RenderableModel::class.java).
         camera.update()
         debugFrustumModel = createFrustumModel(camera)
         debugFrustumInstance = ModelInstance(debugFrustumModel)
+        camera.combined.set(oldCombined)
     }
 }
